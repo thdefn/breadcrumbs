ebd9151f51d8196b48967d8459f7aa7a10c0a75a
haden <80521474+thdefn@users.noreply.github.com>
2023-09-04 18:06:22 +0900

    feat : [001-page] 페이지 정보 조회 엔드포인트
    - bread crumbs 정보를 캐싱
    - raw 쿼리 사용

---
+++ src/main/java/wanted/breadcrumbs/page/PageService.java
@@ -0,0 +1,34 @@
+package wanted.breadcrumbs.page;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+
+@RequiredArgsConstructor
+@Service
+public class PageService {
+    private final PageRepository pageRepository;
+
+    public void create(long parentId, PageForm form) {
+        Long pageId = pageRepository.save(PageVo.builder()
+                .title(form.getTitle())
+                .content(form.getContent())
+                .parentPageId(parentId).build());
+
+        PageCache.addPageBreadCrumb(pageId, parentId);
+    }
+
+    public PageDto read(Long pageId) {
+        PageVo vo = pageRepository.findById(pageId)
+                .orElseThrow(() -> new RuntimeException("no such data exists"));
+
+        return PageDto.builder()
+                .id(vo.getId())
+                .title(vo.getTitle())
+                .content(vo.getContent())
+                .subPages(vo.getSubPages())
+                .breadcrumbs(PageCache.breadcrumb.get(pageId))
+                .build();
+    }
+
+}
